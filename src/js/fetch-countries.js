f44266387eeeb88398fe7e3d9c32343c4e93ad1f
Olya <olya.onysko@gmail.com>
2020-10-06 10:19:35 +0300

    add catch

--- src/js/fetch-countries.js
+++ src/js/fetch-countries.js
@@ -1,6 +1,10 @@
+import error from './notifications';
+
 function fetchCountries(searchQuery) {
   const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
-  return fetch(url).then(res => res.json());
+  return fetch(url)
+    .then(res => res.json())
+    .catch(error => console.log(error));
 }
 
 export default fetchCountries;
